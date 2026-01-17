bc6ce00cae8d60e2df6e160d0d56045b7c3ecfb0
karakurlamia <karakurlamia@gmail.com>
2026-01-17 23:53:23 +0300

    PARETO.sql

--- PARETO.sql
+++ PARETO.sql
@@ -1,4 +1,4 @@
----siparislerin %80 getiren ürünler
+---siparislerin 50 getiren Ã¼rÃ¼nler
 WITH product_orders AS (
 SELECT p.product_id,p.product_name , COUNT(DISTINCT od.order_id)	AS order_count FROM products p
 LEFT JOIN order_details od ON p.product_id=od.product_id
@@ -11,9 +11,9 @@ SELECT product_id,product_name, order_count , SUM(order_count) OVER (ORDER BY or
 SELECT  product_id,product_name ,order_count,cumulative_order,total_orders,
 cumulative_order * 1.0 / total_orders AS cumulative_ratio
 FROM ranked_product
-WHERE cumulative_order * 1.0 / total_orders <= 0.80
+WHERE cumulative_order * 1.0 / total_orders <= 0.50
 ORDER BY order_count DESC;
----cironun %80 getiren ürünler
+---cironun %50 getiren Ã¼rÃ¼nler
 WITH product_ciro AS(
 SELECT p.product_id,p.product_name,COALESCE(SUM(od.quantity*od.Unit_price),0) AS ciro FROM products p
 LEFT JOIN order_details od ON p.product_id=od.product_id
@@ -26,5 +26,6 @@ SUM(ciro) OVER () AS total_ciro  FROM product_ciro
 SELECT product_id,product_name,ciro,cumulative_ciro,total_ciro,
 cumulative_ciro *1.0 / total_ciro AS cumulative_ratio
 FROM ranted_product
-WHERE cumulative_ciro *1.0 / total_ciro <= 0.80
+WHERE cumulative_ciro *1.0 / total_ciro <= 0.50
 ORDER BY ciro DESC
+
